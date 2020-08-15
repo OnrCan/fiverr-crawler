9c3cab21c2a03f62bed5df2a7e9c34e9f14c5929
Onur Can <onrcan.ozkan@gmail.com>
2020-08-15 14:59:51 +0000

    db.js :: remove from async closure

--- db.js
+++ db.js
@@ -1,37 +1,33 @@
 const mongoose = require('mongoose');
 
-(async () => {
+try {
+	mongoose.connect('mongodb://149.28.58.54:27017/fiverr-crawler', { useNewUrlParser: true, useUnifiedTopology: true });
 
-	try {
-		mongoose.connect('mongodb://149.28.58.54:27017/fiverr-crawler', { useNewUrlParser: true, useUnifiedTopology: true });
+	const db = mongoose.connection;
+	db.once('open', _ => {
+		console.log('Database connected:', "mongodb://149.28.58.54:27017/fiverr-crawler")
+	})
 
-		const db = mongoose.connection;
-		db.once('open', _ => {
-			console.log('Database connected:', "mongodb://149.28.58.54:27017/fiverr-crawler")
-		})
+	db.on('error', err => {
+		console.error('connection error:', err)
+	})
 
-		db.on('error', err => {
-			console.error('connection error:', err)
-		})
+	const serviceURLSchema = new mongoose.Schema({
+		categoryURL: String,
+		url: String
+	});
+	const ServiceURL = mongoose.model('ServiceURL', serviceURLSchema);
+	const serviceURLItem = new ServiceURL({
+		categoryURL: "https://fiverr.com",
+		url: "logo-design"
+	});
 
-		const serviceURLSchema = new mongoose.Schema({
-			categoryURL: String,
-			url: String
-		});
-		const ServiceURL = mongoose.model('ServiceURL', serviceURLSchema);
-		const serviceURLItem = new ServiceURL({
-			categoryURL: "https://fiverr.com",
-			url: "logo-design"
-		});
+	serviceURLItem.save(function (err, serviceURLItem) {
+		if (err) return console.error(err);
+	});
 
-		serviceURLItem.save(function (err, serviceURLItem) {
-			if (err) return console.error(err);
-		});
-
-		// db.close();
-	} catch (error) {
-		console.log(error);
-		process.exit();
-	}
-
-})()
+	// db.close();
+} catch (error) {
+	console.log(error);
+	process.exit();
+}
